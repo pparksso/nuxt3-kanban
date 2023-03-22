c32b8369ae7f6c3013ceb366f872b0e7110b3c38
pparksso <thgml5143@hanmail.net>
2023-03-22 10:15:09 +0900

    chore: add nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    modules: ['@pinia/nuxt'],
+    app: {
+        head: {
+            meta: [
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+                {
+                    charset: 'utf-8',
+                },
+            ],
+            title: '칸반보드',
+        },
+    },
+    css: ['@/assets/styles/reset.css'],
+    runtimeConfig: {
+        public: {
+            apiKey: process.env.firebaseKey,
+        },
+    },
+});
